a9a228c8cfba6e56e03a1e990c5122e5c4b45da1
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-12-21 20:39:53 +0200

    Attempt to highlight text with red, to be worked

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -13,17 +13,26 @@ class PostsNew extends Component {
             <div> 
                 <form onSubmit={handleSubmit(this.props.createPost)}>
                     <h3>Create a new post </h3>
-                    <div className="form-group">
+                    <div className="`form-group ${title.touched && title.invalid ? 'has-danger' : '' }`">
                         <label>Title</label>
                         <input type="text" className="form-control" {...title} />
+                        <div className="text-help">
+                            {title.touched ? title.error : ""}
+                        </div>
                     </div>
                     <div className="form-group">
                         <label>Categories</label>
                         <input type="text" className="form-control" {...categories} />
+                        <div className="text-help">
+                            {categories.touched ? categories.error : ""}
+                        </div>
                     </div>
                     <div className="form-group">
                         <label>Content</label>
                         <textarea className="form-control" {...content} />
+                        <div className="text-help">
+                            {content.touched ? content.error : ""}
+                        </div>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                     <Link to="/" className="btn btn-danger">Back</Link> 
@@ -34,7 +43,27 @@ class PostsNew extends Component {
     }
 }
 
+//function to validate fields
+function validate(values) {
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = "Please enter a title";
+    }
+
+    if(!values.categories) {
+        errors.categories = "Please enter a category or categories";
+    }
+
+    if(!values.content) {
+        errors.content = "Please enter content";
+    }
+
+    return errors;
+}
+
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'],
+    validate
 }, null, { createPost })(PostsNew);
